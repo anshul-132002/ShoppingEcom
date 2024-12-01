fe50de800ca68451f0a89cf6f77a6e7c9b9e0863
Anshul shrivas <128448038+anshul-132002@users.noreply.github.com>
2024-12-01 09:39:40 +0530

    loading added

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import { DNA, InfinitySpin, MagnifyingGlass } from "react-loader-spinner";
+import { ShimmerSimpleGallery } from "react-shimmer-effects";
+import { ShimmerDiv } from "shimmer-effects-react";
 
 function Loading() {
   return (
-   
-      <div className="relative w-16 h-16">
-        <div className="absolute inset-0 border-4 border-transparent border-t-blue-500 border-r-blue-500 rounded-full animate-spin"></div>
-        <div className="absolute inset-2 border-4 border-transparent border-t-blue-300 border-r-blue-300 rounded-full animate-spin-reverse"></div>
-      </div>
-   
+    <div className="relative w-16 h-16">
+      {/* <div className="absolute inset-0 border-4 border-transparent border-t-blue-500 border-r-blue-500 rounded-full animate-spin"></div>
+      <div className="absolute inset-2 border-4 border-transparent border-t-blue-300 border-r-blue-300 rounded-full animate-spin-reverse"></div> */}
+     <MagnifyingGlass
+  visible={true}
+  height="80"
+  width="80"
+  ariaLabel="magnifying-glass-loading"
+  wrapperStyle={{}}
+  wrapperClass="magnifying-glass-wrapper"
+  glassColor="#c0efff"
+  color="#e15b64"
+  />
+    </div>
   );
 }
 
--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -59,14 +59,14 @@ function ProductsList() {
       <h2 className="sr-only ">Products</h2>
 
       <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        {products.map((product) => (
+        {products?.map((product) => (
           <div key={product.id} className="group  p-4 rounded-lg">
             <img
               alt={product.title}
               src={product.thumbnail}
               className="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75"
             />
-            <h3 className="mt-4 text-sm text-gray-700 ">{product.title}</h3>
+            <h3 className="mt-4 text-sm text-gray-700  ">{product.title}</h3>
             <p className="mt-1 text-lg font-medium text-gray-900">
               ${product.price}
             </p>
